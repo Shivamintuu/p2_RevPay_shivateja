24b6e838d87bc3b08944322ec1f4933e23b64436
A shivateja <shivamintu964@gmail.com>
2026-06-29 16:25:19 +0530

    Remove unused createdBy and lastModifiedBy fields from Auditable entity

--- src/main/java/com/rev/app/entity/Auditable.java
+++ src/main/java/com/rev/app/entity/Auditable.java
@@ -5,9 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -26,14 +24,6 @@ public abstract class Auditable {
     @LastModifiedDate
     @Column(name = "last_modified_at")
     private LocalDateTime lastModifiedAt;
-
-    @CreatedBy
-    @Column(name = "created_by", updatable = false)
-    private String createdBy;
-
-    @LastModifiedBy
-    @Column(name = "last_modified_by")
-    private String lastModifiedBy;
 
     @Column(name = "deleted", nullable = false)
     private boolean deleted = false;
